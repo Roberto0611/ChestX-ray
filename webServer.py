f52ba1a5056626cd5c939e6dec632d103f3b320a
Roberto0611 <ochoacuevasr@gmail.com>
2024-03-28 16:30:51 -0700

    Model working on the web server :)

--- webServer.py
+++ webServer.py
@@ -20,7 +20,7 @@ def allowed_file(file):
 
 @app.route("/test")
 def test_page():
-    print(prediction("normal.jpeg"))
+    #print(prediction("normal.jpeg"))
     return render_template('test.html')
 
 @app.route('/upload',methods=["POST"])
@@ -32,13 +32,13 @@ def upload():
     if file and allowed_file(filename):
         print('permitido')
         file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-        return render_template('upload_file.html',mensaje="Archivo subido exitosamente :)")
+        return render_template('index.html', filename=filename,result=prediction(f'static/uploads/{filename}'))
     return render_template('upload_file.html',mensaje="no se subio el archivo :(")
 
     return 's'
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    return render_template("index.html", filename='' ,  result="...")
 
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0",port='5000')
