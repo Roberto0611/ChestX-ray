245c2ab048a60dd2c48a9991f16cd0ae3b99a11f
Roberto0611 <ochoacuevasr@gmail.com>
2024-03-27 17:03:53 -0700

    prototype of uploading images to the server

--- webServer.py
+++ webServer.py
@@ -1,7 +1,33 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask("webServer")
 
+app.config["UPLOAD_FOLDER"] = 'static/uploads'
+
+ALLOWED_EXTENSIONS = set(['png',"jpg","jpeg"])
+
+def allowed_file(file):
+    file = file.split('.') # Esto lo convertira en una lista el indice 0 es el nombre y el 1 es el formato
+    if file[1] in ALLOWED_EXTENSIONS:
+        return True
+    else:
+        return False
+
+@app.route('/upload',methods=["POST"])
+def upload():
+    file = request.files.get('uploadFile')
+    print(file,file.filename)
+    filename = secure_filename(file.filename)
+    print(filename)
+    if file and allowed_file(filename):
+        print('permitido')
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+        return render_template('upload_file.html',mensaje="Archivo subido exitosamente :)")
+    return render_template('upload_file.html',mensaje="no se subio el archivo :(")
+
+    return 's'
 @app.route("/")
 def hello_world():
     return render_template("index.html")
